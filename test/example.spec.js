7075a7e640a22b83c89db3ada876f3f026103633
Rafail Ferre <qa.rafailferre@gmail.com>
2024-05-17 02:41:35 -0400

    Installed dependencies and formated

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,8 +1,7 @@
 const { expect } = require('chai');
 
-
-    describe('TESTS', () => {
-        it('Verify math', () => {
-            expect(1 + 2).to.equal(3);
-        });
+describe('TESTS', () => {
+    it('Verify math', () => {
+        expect(1 + 2).to.equal(3);
     });
+});
