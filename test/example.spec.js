700c1accf45608735500524aabb8892551a90e84
Rafail Ferre <154566757+RafailFerre@users.noreply.github.com>
2024-06-02 20:29:09 -0400

    Dependencies (#8)
    * update .prettierrc, example.spec and create .mocharc.js
    
    * create .mocharc.js

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,7 +1,11 @@
-const { expect } = require('chai');
+//const { expect } = require('chai');
+import { expect } from 'chai';
 
 describe('TESTS', () => {
-    it('Verify math', () => {
+    it('Verify math sum', () => {
         expect(1 + 2).to.equal(3);
+    });
+    it('Verify math dev', () => {
+        expect(8 / 2).to.equal(4);
     });
 });
