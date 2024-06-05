7d97a85994a766639de472e501708f9bc319b7ae
Rafail Ferre <154566757+RafailFerre@users.noreply.github.com>
2024-06-05 18:04:40 -0400

    add helpers, modified auth.spec (#11)

--- setup/global.js
+++ setup/global.js
@@ -4,7 +4,7 @@ import request from 'supertest';
 before(async () => {
     const response = await request(process.env.BASE_URL)
         .post('user/login')
-        .send({ email: process.env.Email, password: process.env.PASSWORD });
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD });
 
     process.env.TOKEN = response.body.payload.token;
 });
