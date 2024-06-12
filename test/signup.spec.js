46e0bac60fecb8a06f734736c2a497b68580d973
Rafail Ferre <qa.rafailferre@gmail.com>
2024-06-12 19:54:59 -0400

    updated signup.spec

--- test/signup.spec.js
+++ test/signup.spec.js
@@ -45,7 +45,7 @@ describe('SIGNUP', () => {
             //const newEmail = 'test_' + Date.now() + '@mail.com';
             before(async () => {
                 res = await signup(
-                    '',
+                    '', //first
                     newUser.lastName,
                     newUser.email + 'COM', //newEmail
                     newUser.password
@@ -57,6 +57,63 @@ describe('SIGNUP', () => {
             });
             it('Verify response message', async () => {
                 expect(res.body.message).contain('not created');
+            });
+        });
+        describe('WITH EMPTY LAST NAME FIELD'.blue, () => {
+            //const newEmail = 'test_' + Date.now() + '@mail.com';
+            before(async () => {
+                res = await signup(
+                    newUser.firstName,
+                    '', //lastName
+                    newUser.email + 'COM', //newEmail
+                    newUser.password
+                );
+            });
+            it('Verify response status code', async () => {
+                expect(res.status).to.equal(404);
+                //console.log(res.request._data);
+            });
+            it('Verify response message', async () => {
+                expect(res.body.message).contain('not created');
+            });
+        });
+        describe('WITH EMPTY EMAIL FIELD'.blue, () => {
+            //const newEmail = 'test_' + Date.now() + '@mail.com';
+            before(async () => {
+                res = await signup(
+                    newUser.firstName,
+                    newUser.lastName,
+                    '', //Email
+                    newUser.password
+                );
+            });
+            it('Verify response status code', async () => {
+                expect(res.status).to.equal(404);
+                //console.log(res.body);
+                //console.log(res.request._data);
+            });
+            it('Verify response message', async () => {
+                expect(res.body.message).contain('not created');
+            });
+        });
+        describe.only('WITH EMPTY PASSWORD FIELD'.blue, () => {
+            //const newEmail = 'test_' + Date.now() + '@mail.com';
+            before(async () => {
+                res = await signup(
+                    newUser.firstName,
+                    newUser.lastName,
+                    newUser.email + 'COM', //newEmail
+                    '' //password
+                );
+            });
+            it('Verify response status code', async () => {
+                expect(res.status).to.equal(400);
+                console.log(res.status);
+                console.log(res.body);
+                console.log(res.request._data);
+            });
+            it('Verify response message', async () => {
+                expect(res.body.message).contain('Wrong password format');
             });
         });
     });
