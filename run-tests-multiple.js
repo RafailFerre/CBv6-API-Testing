eeaed4c9c0f7a862dacf75e207f0ea490fb57e04
Rafail Ferre <154566757+RafailFerre@users.noreply.github.com>
2024-06-20 18:41:47 -0400

    Run tests multiple (#19)
    * create file run-testa-multiple.js, add script in package.json
    
    * modified .mocharc.js, update reporterOptions

---
+++ run-tests-multiple.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process');
+
+const runTests = (times) => {
+  for (let i = 1; i <= times; i++) {
+    console.log(`Run #${i}`);
+    try {
+      execSync('npx mocha', { stdio: 'inherit' });
+    } catch (err) {
+      console.error(`Error during run #${i}:`, err);
+      // process.exit(1); // Exit if a test run fails
+    }
+  }
+};
+
+runTests(3);
