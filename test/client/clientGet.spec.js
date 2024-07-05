fc646cea4c5eba36ca75ac267da7fb1e696f06e1
Rafail Ferre <qa.rafailferre@gmail.com>
2024-07-05 02:47:45 -0400

    update complex, client specs, .env.example, prettierrc

--- test/client/clientGet.spec.js
+++ test/client/clientGet.spec.js
@@ -1,29 +1,36 @@
-import { expect } from 'chai';
-import { client, createClient, getClient, searchClient } from '../../helpers/client.js';
-import request from 'supertest';
+import { expect } from 'chai'
+import { client, createClient, deleteClient, getClient, searchClient } from '../../helpers/client.js'
+import request from 'supertest'
 
 describe('GET CLIENT', () => {
-    let res, resGet, resGetAll;
+    let id, res, resGet, resGetAll, resDelete
     before(async () => {
-        res = await createClient(client);
-        const id = res.body.payload;
+        res = await createClient(client)
+        id = res.body.payload
 
-        resGet = await getClient(id);
+        resGet = await getClient(id)
         //console.log(resGet.body.payload._id);
 
-        resGetAll = await searchClient();
+        resGetAll = await searchClient() // get all the clients
+        //console.log(resGetAll.body.payload.items.length);
         //console.log(resGetAll.body.message);
-
-        //console.log(resGetAll.body.payload.items[0]);
+        //console.log(resGetAll.body.payload.items);
+    })
+    after(async () => {
+        resDelete = await deleteClient(id)
+        //console.log(resDelete.body);
     })
 
     it('verify status code when trying to get client', () => {
-        expect(resGet.status).to.equal(200);
-    });
+        expect(resGet.status).to.equal(200)
+    })
     it('verify message when trying to get client', () => {
-        expect(resGet.body.message).to.equal('Get Client by id ok');
-    });
+        expect(resGet.body.message).to.equal('Get Client by id ok')
+    })
+    it('verify total clients when trying to get clients', () => {
+        expect(resGetAll.body.message).to.equal('ClientSearch ok')
+    })
     it('verify id when trying to get client', () => {
-       expect(resGetAll.body.payload.items[0]._id).to.equal(resGet.body.payload._id);
-    });
-});
+        expect(resGetAll.body.payload.items[0]._id).to.equal(resGet.body.payload._id)
+    })
+})
