4788139bf6f5b9fec013a1b21d6577e28b2dbe06
Rafail Ferre <qa.rafailferre@gmail.com>
2024-07-17 01:06:36 -0400

    update global, set new variant Authorization for create clien

--- setup/global.js
+++ setup/global.js
@@ -8,3 +8,16 @@ before(async () => {
 
     process.env.TOKEN = response.body.payload.token;
 });
+
+
+before(async () => {
+    const resLoginToken = (await request(process.env.BASE_URL)
+        .post('/user/login')
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })).body.payload.token
+
+    const resAuth = await request(process.env.BASE_URL)
+        .get('/user/auth')
+        .set('Authorization', resLoginToken);
+
+    process.env.AUTHORIZATION = resAuth.request.header['Authorization'];
+});
